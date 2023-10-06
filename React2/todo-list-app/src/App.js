6e51bcfbdc35f801b0d52e42862dc85cd96467ba
Hunter7210 <heitorrmsilva@gmail.com>
2023-10-06 15:23:07 -0300

    06/10/2023

--- React2/todo-list-app/src/App.js
+++ React2/todo-list-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import TodoList from './TodoList';
+
 function App() {
   return (
     <div className="App">
-      <TodoList />
+    <TodoList />
     </div>
   );
 }
